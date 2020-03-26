2fcbdf01c850fcda1bd320b4e58892df80ca5a70
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 21:20:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
- your_hash = {"name" => "Maria", "age" => "29", "proffesion" => "future developer"}
+ your_hash = {"name" => "Maria", "age" => 29 , "proffesion" => "future developer"}
 
   
 end

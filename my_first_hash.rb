939f45c1c47843e5cec436f1a5e2dca5415dad9c
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 21:30:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,12 +26,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest = ["muskets"]
-  shipping_manifest =["gun powder"]
+  shipping_manifest ["muskets"] = 2
+  shipping_manifest ["gun powder"] = 4
 
 
-
-
-  # return the shipping_manifest hash below
+shipping_manifest
 
 end

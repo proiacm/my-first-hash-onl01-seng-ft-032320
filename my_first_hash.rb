dcc64213765b85374ca0ab322acdebbbc83ee89b
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 21:23:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,11 @@
 def my_hash
  your_hash = {"name" => "Maria", "age" => 29 , "proffesion" => "future developer"}
 
-  
 end
 
 
 def shipping_manifest
  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-
 
 end
 

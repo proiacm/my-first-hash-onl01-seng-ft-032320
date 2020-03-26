ccb9dc1ffbdf236a164ba90351ef121c8ebc9976
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 21:21:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,8 +6,7 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
+ the_manifest = { 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
 
